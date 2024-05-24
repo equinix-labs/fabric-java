a6ea03d5fdaeb76917a7cb3a83af73a487df1e7e
Tomasz Tutka <tomasz.tutka@eu.equinix.com>
2024-05-24 19:11:36 +0200

    corrected imports for junit

--- v4/api/PricesApiTest.java
+++ v4/api/PricesApiTest.java
@@ -20,8 +20,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
  * API tests for PricesApi
